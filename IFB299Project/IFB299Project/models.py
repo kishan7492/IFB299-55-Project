55e427c8072f72394f2c9bf9048e1c016a75f647
kishan <kishan7492@gmail.com>
2017-10-05 10:00:56 +1000

    Signin fix

--- IFB299Project/IFB299Project/models.py
+++ IFB299Project/IFB299Project/models.py
@@ -15,10 +15,21 @@ class Page(models.Model):
     def __unicode__(self):
         return self.title
 
-class Pageee(models.Model):
-    category = models.ForeignKey(Category)
-    title = models.CharField(max_length=128)
-    url = models.URLField()
-    views = models.IntegerField(default=0)
+class Placeinformation(models.Model):
+    Placename = models.CharField(max_length = 300)
+    address = models.CharField(max_length = 400)
+    description = models.CharField(max_length = 400)
+    long = models.CharField(max_length=15)
+    lat = models.CharField(max_length=15)
+    typeOfPlace = models.CharField(max_length=15)
     def __unicode__(self):
-        return self.title
+        return self.Placename
+
+class users(models.Model):
+    userID = models.CharField(max_length = 300)
+    Username = models.CharField(max_length = 400)
+    Email = models.EmailField
+    name = models.CharField(max_length=30)
+    typwOfUser = models.CharField(max_length=15)
+    def __unicode__(self):
+        return self.userID
