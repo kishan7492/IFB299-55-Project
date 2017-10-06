e78a52d2afdc11c0fab0d3c0f477fcc3fbe04cb9
kishan7492 <kishan7492@gmail.com>
2017-10-06 12:49:16 +1000

    Add files via upload

---
+++ IFB299Project/IFB299Project/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from IFB299Project.models import Category, Page, Placeinformation, users
+
+
+admin.site.register(Category)
+admin.site.register(Page)
+admin.site.register(Placeinformation)
+admin.site.register(users)
