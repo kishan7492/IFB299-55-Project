4b5d75e2e1dcc352227c12cc2cbcd844044e9dd3
arshdaudhar <arshdaudhar@gmail.com>
2017-10-25 12:20:01 +1000

    Create Account Changes

---
+++ IFB299Project/IFB299Project/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Enter your full name')
+
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Enter your full name')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name','last_name', 'email', 'password1', 'password2', )
